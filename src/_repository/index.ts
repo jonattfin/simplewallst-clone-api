03e8ba146544d7c1984cbcd06f3692c0363a56b7
jonattfin <jonattfin@outlook.com>
2022-07-22 20:33:55 +0300

    refactored graph ql

--- src/_repository/index.ts
+++ src/_repository/index.ts
@@ -1,4 +1,11 @@
-import { Company, News, Stock } from 'src/_shared/entities';
+import {
+  Company,
+  News,
+  Stock,
+  StockPrice,
+  Reward,
+  Risk,
+} from 'src/_shared/entities';
 
 export default class DbRepository {
   private readonly _company: Company;
@@ -29,6 +36,23 @@ export default class DbRepository {
       name: 'ING Groep',
       description:
         'ING Groep N.V., a financial institution, provides various banking products and services in the Netherlands, Belgium, Germany, Poland, Rest of Europe, North America, Latin America, Asia, and Australia.',
+      rewards: [
+        new Reward({
+          id: 1,
+          description: 'Trading at 67% below our estimate of its fair value',
+        }),
+        new Reward({
+          id: 2,
+          description: 'Earnings are forecast to grow 12.35% per year',
+        }),
+        new Reward({
+          id: 3,
+          description: 'Earnings grew by 48.9% over the past year',
+        }),
+      ],
+      risks: [
+        new Risk({ id: 1, description: 'Unstable dividend track record' }),
+      ],
     });
   }
 
@@ -37,15 +61,22 @@ export default class DbRepository {
       id: 1,
       companyId,
       ticker: 'INGA',
+      exchangeName: 'ENXTAM',
       lastPrice: 9.18,
       marketCap: 34.2,
       priceSevenDays: 1.9,
       priceOneYear: -13.1,
       lastUpdated: '20 Jul, 2022',
+      // history: [new StockPrice({ id: 1 })],
     });
   }
 
   _getNews(companyId: number) {
-    return new News({ id: 1, companyId, date: 'd1', text: 'a' });
+    return new News({
+      id: 1,
+      companyId,
+      date: new Date(2022, 5, 18).toDateString(),
+      text: 'ING Groep N.V. commences an Equity Buyback Plan, under the authorization approved on April 25, 2022.',
+    });
   }
 }
